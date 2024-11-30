7b5ba26be139a9dc03bb794247af5d0954389792
Nuttawat Chantraphakorn <11488608@krungsri.com>
2024-12-01 02:24:04 +0700

    Add Dockerfile

--- frontend/core/config.py
+++ frontend/core/config.py
@@ -1,5 +1,6 @@
 # API Configuration
-API_URL = "http://localhost:7890"
+# API_URL = "http://localhost:7890"
+API_URL = "http://host.docker.internal:7890"
 
 # Graph Visualization Settings
 GRAPH_OPTIONS = {
